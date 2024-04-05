b9d7cadfcf7cd4bbc814d6a467709704fbfa89c6
Syarif <syarif.nur92@gmail.com>
2024-04-05 07:00:40 +0700

    Update HistoryFragment.kt

--- app/src/main/java/com/dicoding/asclepius/view/history/HistoryFragment.kt
+++ app/src/main/java/com/dicoding/asclepius/view/history/HistoryFragment.kt
@@ -1,6 +1,5 @@
 package com.dicoding.asclepius.view.history
 
-import com.dicoding.asclepius.data.local.Resource
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.asclepius.data.local.Resource
 import com.dicoding.asclepius.databinding.FragmentHistoryBinding
 import com.dicoding.asclepius.view.result.ResultActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
